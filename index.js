5d733cb4448cfbe1a2b35c21ee2459a474c579d6
Heather <unphurlng@gmail.com>
2018-10-03 20:43:01 -0700

    show prompt and confirm

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello bspreppers!!!!');
+var countTo = prompt('how high should we count even numbers?');
+
+// (init; condition; post-loop)
+if(sure) {
+    for(var i = 0; i < countTo; i++) {
+        var y = i % 2;
+        if(y === 0) {
+            console.log('the number', i, 'is under', countTo);
+        }
+}
